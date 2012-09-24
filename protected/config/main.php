bf4d1fc3642c6e35103af71bee28f850a6d1c480
luckymancvp <luckymancvp@gmail.com>
2012-09-24 11:01:23 +0700

    Add global file

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,6 +5,9 @@
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'globals.php';
+
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
@@ -19,15 +22,12 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'2e',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -36,30 +36,22 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+            'showScriptName' => false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=vocal',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'root',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -67,16 +59,16 @@ return array(
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
+                    /*
+                    array(
+                        'class'=>'CFileLogRoute',
+                        'levels'=>'error, warning',
+                    ),
+                    // uncomment the following to show log messages on web pages
+                    array(
+                        'class'=>'CWebLogRoute',
+                    ),
+                    */
 			),
 		),
 	),
@@ -87,4 +79,5 @@ return array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
 	),
+    'theme'=>'wuxia',
 );
--- protected/config/test.php
+++ protected/config/test.php
@@ -1,17 +1,32 @@
 <?php
 
 return CMap::mergeArray(
-	require(dirname(__FILE__).'/main.php'),
-	array(
-		'components'=>array(
-			'fixture'=>array(
-				'class'=>'system.test.CDbFixtureManager',
-			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
-		),
-	)
+    require(dirname(__FILE__).'/main.php'),
+    array(
+        'components'=>array(
+            'urlManager'=>array(
+                'showScriptName'=>true,
+            ),
+            'db'=>array(
+                'enableProfiling'=>true,
+            ),
+            'fixture'=>array(
+                'class'=>'system.test.CDbFixtureManager',
+            ),
+            'log'=>array(
+                'class'=>'CLogRouter',
+                'routes'=>array(
+                    //テストのためブラウザでひょうじする
+                    array(
+                        'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
+                    ),
+                ),
+            ),
+            /* uncomment the following to provide test database connection
+               'db'=>array(
+                   'connectionString'=>'DSN for test database',
+               ),
+               */
+        ),
+    )
 );
