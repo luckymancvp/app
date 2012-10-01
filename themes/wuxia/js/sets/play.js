ceb7a1ba6daf6d1386f6b89952b773af8a95221f
luckymancvp <luckymancvp@gmail.com>
2012-10-01 17:24:52 +0700

    add simple play

---
+++ themes/wuxia/js/sets/play.js
@@ -0,0 +1,123 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: luckymancvp
+ * Date: 10/1/12
+ * Time: 2:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function(){
+
+    $("#quiz-progress").hide();
+    // Event listener
+    $("#start-quiz").click(function(){
+        $("#quiz-setup").fadeOut("fast",function(){$("#quiz-progress").fadeIn("slow");});
+
+        quizView.start();
+        return false;
+    });
+
+    // Use backbone library
+    var Item  = Backbone.Model.extend({
+        defaults: {
+            word: "",
+            meaning : "",
+            sound: ""
+        }
+    });
+
+    var Items = Backbone.Collection.extend({
+        model : Item
+    });
+
+    var QuizView = Backbone.View.extend({
+        el           : "#quizZone",
+        template     : $("#itemQuiz").html(),
+        templateEnd  : $("#quizEnd").html(),
+        events : {
+            "click .yes"      : "yes",
+            "click .no"       : "no",
+            "click .continue" : "continue"
+        },
+        loop  : 0,
+        count : 0,
+        next  : 0,
+        orderedIems: new Array(),
+        initialize: function(){
+            this.collection = new Items(itemsJSON);
+        },
+        render       : function(){
+            var progress = Math.round(this.next*1000.0 / this.count) / 10;
+            $("#progress-bar").html(progress + "%");
+            $("#progress-bar").attr("style","width: "+progress+"%");
+
+            if (this.next == this.count){
+                this.loop++;
+                $("#loop").html(this.loop);
+                this.renderEnd();
+            }
+            else
+                this.renderQuiz(this.orderedItems[this.next++]);
+        },
+        'start': function(){
+            this.order();
+
+            // display init state
+            $("#loop").html(0);
+            $("#true").html(0);
+            $("#wrong").html(0);
+            $("#total").html(this.count);
+
+            this.next    = 0; // next index item
+            this.render();
+        },
+        order : function(){
+            // init
+            this.orderMode = "sequence";
+            that = this;
+            this.orderedItems = new Array();
+            this.count = 0;
+            switch (this.orderMode){
+                case "sequence":
+                    _.each(this.collection.models, function(item){
+                        that.orderedItems[that.count++] = item;
+                    });
+                    break;
+            }
+        },
+        renderQuiz : function(item){
+            var tmpl = _.template(this.template);
+            this.$el.html(tmpl(item.toJSON()));
+            return this;
+        },
+        renderEnd : function(){
+            var tmpl = _.template(this.templateEnd);
+            this.$el.html(tmpl({loop: this.loop}));
+            return this;
+        },
+        yes : function(e){
+            e.preventDefault();
+
+            $("#true").html(parseInt($("#true").html()) + 1);
+            this.render();
+        },
+        no : function(e){
+            e.preventDefault();
+
+            $("#wrong").html(parseInt($("#true").html()) + 1);
+            this.render();
+        },
+        continue: function(e){
+            e.preventDefault();
+
+            $("#true").html(0);
+            $("#wrong").html(0);
+
+            this.next = 0;
+            this.render();
+        }
+    });
+
+    quizView = new QuizView();
+    //$("#start-quiz").click();
+});
---
+++ themes/wuxia/views/sets/play.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: luckymancvp
+ * Date: 10/1/12
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ *
+ * @var $this SetsController
+ * @var $model Items
+ * @var $set   Sets
+ */
+
+    //  Js file
+    Yii::app()->clientScript->registerScriptFile(Html::jsThemeUrl("libs/underscore-min.js"));
+    Yii::app()->clientScript->registerScriptFile(Html::jsThemeUrl("libs/backbone-min.js"));
+    Yii::app()->clientScript->registerScriptFile(Html::jsThemeUrl("sets/play.js"));
+?>
+
+<script>
+    var itemsJSON    = <?php echo CJSON::encode($set->items)?>;
+</script>
+
+<script id="itemQuiz" type="text/template">
+    <div class="row">
+        <div class="hero-unit blow span4 offset3">
+            <h1><%=word%> !</h1>
+            <p>Do you know this word ?</p>
+            <button class="btn btn-inverse no">No</button>
+            <button class="btn btn-success yes">Yes</button>
+        </div>
+    </div>
+</script>
+
+<script id="quizEnd" type="text/template">
+    <div class="row">
+        <div class="hero-unit blow span4 offset3">
+            <div class="nav-secondary">
+                <nav>
+                    <ul>
+                        <li><a href="#" class="wuxify-me continue"><span class="icon-heart"></span>Continue</a><span class="badge badge-inverse"><%=loop%></span></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</script>
+
+<div class="content" id="quiz-setup">
+
+    <!-- Page header -->
+    <div class="page-header">
+        <h1>Welcome, Let's play and learn!</h1>
+        <ul class="page-header-actions">
+            <li><a class="btn btn-wuxia btn-success pull-right" href="#" data-original-title="Start Quiz" id="start-quiz"><span class="icon-bookmark"></span> Play Now</a></li>
+        </ul>
+    </div>
+    <!-- /Page header -->
+
+    <!-- Page container -->
+    <div class="page-container">
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Page grid cell (4 blocks) -->
+            <article class="span4">
+                <p class="lead">Select play mode</p>
+            </article>
+            <!-- /Page grid cell (4 blocks) -->
+
+            <!-- Page grid cell (8 blocks) -->
+            <article class="span8">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="control-group">
+                            <label for="select" class="control-label">Defined Mode</label>
+                            <div class="controls">
+                                <select id="select">
+                                    <option>All</option>
+                                    <option>Medium</option>
+                                    <option>Hard</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <label class="control-label">List Order</label>
+                            <div class="controls">
+                                <label class="radio">
+                                    <input type="radio" checked="checked" value="seq" name="optionRadio">
+                                    Sequence
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" value="ran" name="optionRadio">
+                                    Random
+                                </label>
+                            </div>
+                        </div>
+
+                    </fieldset>
+                </form>
+            </article>
+            <!-- /Page grid cell (8 blocks) -->
+
+        </div>
+        <!-- /Grid row -->
+
+    </div>
+    <!-- /Page container -->
+
+</div>
+
+<div class="content" id="quiz-progress">
+
+    <!-- Page header -->
+    <div class="page-header">
+        <h1>Play Progress</h1>
+        <ul class="page-header-actions">
+            <!--<li><a class="btn btn-wuxia btn-success pull-right" href="#" data-original-title="Start Quiz" id="start-quiz"><span class="icon-bookmark"></span> Play Now</a></li>-->
+        </ul>
+    </div>
+    <!-- /Page header -->
+
+    <!-- Page container -->
+    <div class="page-container">
+
+        <!-- Grid row -->
+        <div class="row">
+            <div class="span6">
+                <div style="margin-bottom: 9px;" class="progress progress-success progress-striped">
+                    <div style="width: 0%" class="bar" id="progress-bar">0%</div>
+                </div>
+            </div>
+            <div class="span3">
+                <div>
+                    Loop: <div id="loop" style="display: inline-block;">0</div> | True : <div id="true" style="display: inline-block;">0</div> | Wrong : <div id="wrong" style="display: inline-block;">0</div> | Total : <div id="total" style="display: inline-block;">0</div>
+                </div>
+            </div>
+        </div>
+        <!-- /Grid row -->
+
+    </div>
+    <!-- /Page container -->
+
+</div>
+
+<div id="quizZone"></div>
